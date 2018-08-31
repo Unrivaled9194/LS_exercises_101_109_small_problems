0f8457f1782b6aff389ddeb3f69ae6d28331d18d
Matthew Guyatt <mguyatt@Matthews-MacBook-Pro.local>
2018-08-31 11:47:30 -0400

    first commit for 101_109 exercises

---
+++ easy_1/1_repeat_yourself.rb
@@ -0,0 +1,7 @@
+# 1_repeat_yourself.rb
+
+def repeat(string_input, times_to_print)
+  times_to_print.times {puts string_input}
+end
+
+repeat('Hello Bob', 5)
---
+++ easy_1/2_odd.rb
@@ -0,0 +1,17 @@
+# 2_odd.rb
+
+def is_odd?(number)
+  number % 2 == 1
+end
+
+# Further exploration
+# def is_odd?(number)
+#   number.remainder(2) == 1
+# end
+
+puts is_odd?(2)
+puts is_odd?(5)
+puts is_odd?(-17)
+puts is_odd?(-8)
+puts is_odd?(0)
+puts is_odd?(7)
